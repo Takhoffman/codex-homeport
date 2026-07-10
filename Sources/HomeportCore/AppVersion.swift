7dfcbb4fe9b3918a490607c24230e4b1339d1a4d
Tak Hoffman <781889+Takhoffman@users.noreply.github.com>
2026-07-09 23:16:41 -0500

    Release v0.11.0

--- Sources/HomeportCore/AppVersion.swift
+++ Sources/HomeportCore/AppVersion.swift
@@ -1,4 +1,4 @@
 public enum AppVersion {
-    public static let version = "0.10.0"
-    public static let build = "19"
+    public static let version = "0.11.0"
+    public static let build = "20"
 }
